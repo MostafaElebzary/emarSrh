7efa39356eb579a903d58b917df568adb7e3440f
MostafaElebzary <mostafaelebzary@gmail.com>
2021-10-30 18:56:40 +0200

    m

---
+++ app/Exports/RecieptsExcel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Exports;
+ 
+use App\Reciept;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+class RecieptsExcel implements FromView
+{
+    use Exportable;
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function __construct($pay_type ,$fromdate,$todate,$type)
+    {
+        $this->pay_type = $pay_type;
+        $this->fromdate = $fromdate;
+        $this->todate = $todate;
+        $this->type = $type;
+    }
+    public function view(): View
+    {
+
+        if ($this->pay_type == 'all') {
+            $reciepts = Reciept::whereBetween('date', array($this->fromdate, $this->todate))
+                ->where('type', $this->type)
+                ->get();
+        } else {
+            $reciepts = Reciept::whereBetween('date', array($this->fromdate, $this->todate))
+                ->where('type', $this->type)->where('pay_type', $this->pay_type)
+                ->get();
+        }
+
+            return view('recipts.excel', [
+                'reciepts' => $reciepts,
+            ]);
+
+    }
+}
